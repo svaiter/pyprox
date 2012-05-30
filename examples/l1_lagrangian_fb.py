aec77ac7c21ca31433872f762e780ad0d1e0e0d0
Samuel Vaiter <samuel.vaiter@gmail.com>
2012-05-30 20:03:34 +0200

    Initial example : Lasso for CS with Forward-backward

---
+++ examples/l1_lagrangian_fb.py
@@ -0,0 +1,55 @@
+"""
+=================================================================
+Basis Pursuit Denoising with Forward-Backward : CS Regularization
+=================================================================
+
+Test the use of Forward-backward-like splitting for the resolution of a
+compressed sensing regularization
+"""
+
+# modules
+from __future__ import division
+import numpy as np
+import pylab as plt
+
+from pyprox.algorithms import forward_backward
+from pyprox.utils import soft_thresholding
+
+n = 600
+p = n//4
+la = 1.0 # regularization parameter
+
+# Matrix and observations
+A = np.random.randn(p,n)
+y = np.random.randn(p,1)
+
+# List of benchmarked algorithms
+methods = ['fb', 'fista', 'nesterov']
+
+# operator callbacks
+F = lambda x: la*np.linalg.norm(x,1)
+G = lambda x: 1/2*np.linalg.norm(y - np.dot(A,x)) ** 2
+ProxF = lambda x,tau: soft_thresholding(x, la*tau)
+GradG = lambda x: np.dot(A.T,np.dot(A,x) - y)
+
+L = np.linalg.norm(A, 2) ** 2 #Lipschitz constant
+
+callback = lambda x: F(x) + G(x)
+maxiter = 1000
+
+res = np.zeros((maxiter,len(methods)))
+i = 0
+for method in methods:
+    x, fx = forward_backward(ProxF, GradG, np.zeros((n,1)), L,
+        maxiter=maxiter, method=method,
+        full_output=1, retall=0, callback=callback)
+    res[:,i] = fx
+    i += 1
+
+e = np.min(res.flatten())
+
+plt.loglog(res[:(maxiter // 10),:] - e)
+plt.legend(methods)
+plt.grid(True,which="both",ls="-")
+plt.tight_layout()
+plt.show()
