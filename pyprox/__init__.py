26248a4f6b1ac27e770b8d9f138f6c65e98a3e97
Samuel Vaiter <samuel.vaiter@gmail.com>
2012-06-02 14:10:31 +0200

    Simple change in examples

--- pyprox/__init__.py
+++ pyprox/__init__.py
@@ -1,5 +1,5 @@
 from pyprox.algorithms import douglas_rachford, forward_backward, admm
-#from pyprox.utils import soft_thresholding, dual_prox
+from pyprox.utils import soft_thresholding, dual_prox
 
 __all__ = ['algorithms','datasets','utils']
 __version__ = "0.1"
